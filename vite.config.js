41227ffeeb22c4e941d235239a4dc59e0c786e77
Yi-Jing <meg778899@gmail.com>
2024-08-08 09:47:32 +0800

    feat: 加入 base

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/test-bird/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
